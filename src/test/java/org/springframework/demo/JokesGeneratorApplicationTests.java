0340c51f0630e2500a660ac22f6ceda2ed456aa7
jnfviana <joao_nsviana@hotmail.com>
2020-04-21 18:49:23 +0100

    Project Commit

---
+++ src/test/java/org/springframework/demo/JokesGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
