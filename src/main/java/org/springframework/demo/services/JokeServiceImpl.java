bbd345c62bd74aac2b4a427d95fe0f16f5d74586
jnfviana <joao_nsviana@hotmail.com>
2020-04-23 18:31:56 +0100

    Java Configuration implementation

--- src/main/java/org/springframework/demo/services/JokeServiceImpl.java
+++ src/main/java/org/springframework/demo/services/JokeServiceImpl.java
@@ -8,8 +8,8 @@ public class JokeServiceImpl extends guru.springframework.norris.chuck.ChuckNorr
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
